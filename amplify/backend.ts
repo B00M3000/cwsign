29e0283410072b3f6554db044e17ea0d95cad108
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2026-05-08 16:32:14 +0000

    change to work with non static

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,9 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { FunctionUrlAuthType } from 'aws-cdk-lib/aws-lambda';
-import { stocksApi } from './stocks-api/resource';
+import { stocksApi } from './functions/stocks-api/resource';
 
 const backend = defineBackend({ stocksApi });
 
-// Expose the function as a public HTTPS endpoint with CORS
 backend.stocksApi.resources.lambda.addFunctionUrl({
   authType: FunctionUrlAuthType.NONE,
   cors: { allowedOrigins: ['*'] },
